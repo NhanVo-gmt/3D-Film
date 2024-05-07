f648ac3e8b45835214e71143559f5562b7f5c7a8
Ryunosuke M <111721351+Ryunosuke39@users.noreply.github.com>
2024-05-08 00:28:49 +1000

    act3-10 updated
    added IK to man's model, showing device

--- Assets/Act3-Ryunosuke/Act3-script/Act38deviceHolder.cs
+++ Assets/Act3-Ryunosuke/Act3-script/Act38deviceHolder.cs
@@ -6,6 +6,7 @@ public class Act38deviceHolder : MonoBehaviour
 {
     Animator anim;
     public GameObject target;
+    public GameObject hand;
     public float IK_weight = 1.0f;
 
     // Start is called before the first frame update
@@ -15,9 +16,13 @@ public class Act38deviceHolder : MonoBehaviour
     }
 
     private void OnAnimatorIK(int layerIndex){
-        // attaching teh device to hand
+        // pointing hand to device
         anim.SetIKPosition(AvatarIKGoal.RightHand, target.transform.position);
         anim.SetIKPositionWeight(AvatarIKGoal.RightHand, IK_weight);
+        // attach device to hand
+        target.transform.parent = hand.transform;
+        target.transform.localPosition = new Vector3(0.00562f, -0.00031f, -0.00119f);
+        target.transform.localRotation = Quaternion.Euler(-13.075f, 370.383f, -65.793f);
     }
 
     // Update is called once per frame
